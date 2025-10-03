af3daf684bf36bbdce37b5dc3a8d17375ae43a60
Vishal Kumar <128839643+Vishal586@users.noreply.github.com>
2025-10-03 21:05:59 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://taskapp-backend2.onrender.com/api';
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
@@ -28,4 +28,5 @@ api.interceptors.response.use(
         }
         return Promise.reject(error);
     }
+
 );
